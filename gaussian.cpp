56202e35ff59629cdbb24c029e08f1de180ef916
harshanabandara <e16049@eng.pdn.ac.lk>
2022-02-17 18:33:23 +0530

    changed the code to see time complexity

--- gaussian.cpp
+++ gaussian.cpp
@@ -3,10 +3,11 @@
 #include <ctime>
 #include <cmath>
 #include "sodium.h"
+#include <chrono>
 
 #define PI 3.14
-#define q 1000
-#define n 30
+#define q 12289
+#define n 1024
 #define m 270
 using namespace std;
 
@@ -29,15 +30,20 @@ int main(int argc, char const *argv[])
     
     double alpha = sqrt(double(n))/q;
     double sigma = alpha/sqrt(2*PI);
-    std::mt19937 gen(randombytes_random());    
-    std::normal_distribution<double> gauss_dis{0,sigma};
+    // double sigma = sqrt(1.75);
+    // std::mt19937 gen(randombytes_random());    
+    normal_distribution<double> gauss_dis{0,sigma};
 
     int count = 0;
+    chrono::steady_clock sc;
+    auto start = sc.now();
     while (count <100000)
     {
-        cout<<gaussian(sigma)<<endl;
+        gaussian(sigma);
         count++;
     }
-       
+    auto end = sc.now();
+    auto time_span = static_cast<chrono::duration<double>>(end - start);   // measure time span between start & end
+    cout<<"Operation took: "<<time_span.count()<<" seconds !!!";
     return 0;
 }
