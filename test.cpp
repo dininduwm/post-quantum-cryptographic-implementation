c5066ce825a3af0c0637bb1e0c66a5257bdd82a4
harshanabandara <e16049@eng.pdn.ac.lk>
2022-03-05 16:09:14 +0530

    byte ranges

--- test.cpp
+++ test.cpp
@@ -1,26 +1,38 @@
+#include <climits>
 #include <iostream>
-// #include "./AESHelper/AESHelper.h"
-#include <cstddef>
+#include "sodium.h"
 
+typedef long long dtype;
 using namespace std;
-short * binConvert(byte* input,int AESKeyLength) 
+dtype genUniformRandomlong(dtype lowerBound, dtype upperBound)
 {
-
-  short*  bitstream = new short[AESKeyLength * 8];
-
-  
-  for (int i = 0; i < AESKeyLength; ++i)
-   {
-	for (int j = 0; j < 8; ++j) 
-		{
-		  bitstream[ (i*8 - j + 7)] = (input[i] >> j) & 1;    
-		}
-   } 
-   
-   return bitstream;
+    dtype range = (upperBound - lowerBound) + 1;
+    dtype randomNumber;
+    randomNumber = randombytes_uniform(range);
+    dtype randomNumberModified = ((dtype)randomNumber) + lowerBound;
+    return randomNumberModified;
 }
 
-int main(){
-    std::cout<<"Fine\n";
-    return 0;
+int main(int argc, char const *argv[])
+{
+  dtype q = 1<<30;
+  // unsigned long tmp1 = 1;
+  // tmp1 = tmp1 << 31;
+  // q = tmp1 - 19;
+  // q = LLONG_MAX;
+  // q = q>>5;
+  long s = LONG_MAX;
+  cout<<s<<endl;
+  cout<<LLONG_MAX<<endl;
+  cout<<INT32_MAX<<endl;
+  cout<<sizeof(long long)<<endl;
+  cout<<sizeof(long)<<endl;
+  cout<<sizeof(int)<<endl;
+  cout<<q<<endl;  
+  cout<<hex<<q<<endl;
+  // for (int i = 0; i < 1000000; i++)
+  // {
+  //   // printf(genUniformRandomlong(0,q-1));
+  // }
+  return 0;
 }
