4142311d5d66892095e3020f4f8299bf9dac6e7e
harshanabandara <e16049@eng.pdn.ac.lk>
2022-03-21 16:19:47 +0530

    aes_prng

--- keygen.cpp
+++ keygen.cpp
@@ -14,20 +14,46 @@ randombytes_buf(&seed,sizeof(seed))
 */
 
 #include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+#include <cstdint>
+// using std::bitset;
 #include <bitset>
 #include <crypto++/aes.h>
-#include "functions.h"
+using CryptoPP::AES;
 #include <crypto++/modes.h>
+using CryptoPP::ECB_Mode;
 #include "sodium.h"
 #include <boost/multiprecision/cpp_int.hpp>
-
-using CryptoPP::AES;
-using CryptoPP::ECB_Mode;
 using namespace boost::multiprecision;
-using namespace std;
+#include "crypto++/osrng.h"
+using CryptoPP::AutoSeededRandomPool;
+#include "crypto++/cryptlib.h"
+using CryptoPP::Exception;
+#include "crypto++/hex.h"
+using CryptoPP::HexEncoder;
+using CryptoPP::HexDecoder;
 
-#define n 30
-#define m 370
+#include "crypto++/filters.h"
+using CryptoPP::StringSink;
+using CryptoPP::StringSource;
+using CryptoPP::StreamTransformationFilter;
+
+#include <string>
+using std::string;
+// typedef unsigned char byte
+
+#define n 10
+#define m 5
+
+union un{
+    byte buff[16];
+    // std::bitset<128> bitset_buff;
+    int int_buf[4];
+    int16_t short_buff[8];
+    char char_buf[16];
+};
 
 int128_t byteToint(std::byte seed[16]){
     int128_t retU = -1;
@@ -36,25 +62,6 @@ int128_t byteToint(std::byte seed[16]){
     return retU;
 }
 
-string ECBMode_Encrypt(string text, byte key[], int keySize) {
-    string cipher = "";
-    //Encryption
-    try
-    {
-        ECB_Mode<AES>::Encryption e;
-        e.SetKey(key, keySize);
-        // The StreamTransformationFilter adds padding
-        //  as required. ECB and CBC Mode must be padded
-        //  to the block size of the cipher.
-        StringSource(text, true, new StreamTransformationFilter(e, new StringSink(cipher))); // StringSource
-    }
-    catch(const CryptoPP::Exception& e)
-    {
-        cerr << e.what() << endl;
-        exit(1);
-    }
-    return cipher;
-}
 
 void printArray(int16_t array[8]){
     cout<<"[ ";
@@ -68,9 +75,10 @@ void printArray(int16_t array[8]){
 
 
 
+
 int main(int argc, char const *argv[])
 {
-    bitset<128>seed;
+    std::bitset<128>seed;
     cout<<sizeof(seed)<<endl;
     randombytes_buf(&seed,sizeof(seed));
     // coutzz<<hex<<seed
@@ -83,8 +91,9 @@ int main(int argc, char const *argv[])
         cout<<seed[127-i];
     }
     cout<<endl;
-    printArray(b);
+    // printArray(b);
     int16_t i,j;
+    /*
     for (i = 0; i < 10; i++)
     {
         for(j=0;j<16;j=j+8)
@@ -103,11 +112,130 @@ int main(int argc, char const *argv[])
             
         }
     }
-    cout<<sizeof(i)<<sizeof(j);
+    */
+    cout<<sizeof(i)<<sizeof(j)<<endl;
 
+    AutoSeededRandomPool prng;
+    union un u1;
+    byte key[AES::DEFAULT_KEYLENGTH];
+    cout<<AES::DEFAULT_KEYLENGTH<<endl;
+    prng.GenerateBlock(u1.buff,sizeof(u1.buff));
+    string plain = "Hello crypto1243";
+    // cout<<seed<<endl;
+    // memcpy(&key,&seed,sizeof(key));
+    // cout<<u1.short_buff<<endl;
+    printArray(u1.short_buff);
+    string cipher,encoded,recovered;
+    memcpy(&u1.buff,&key,sizeof(key));
+    encoded.clear();
+    StringSource(key,sizeof(key),true,new HexEncoder(new StringSink(encoded)));
+    cout<<"key: "<<encoded<<" "<<sizeof(key)<<endl;
+    cout<<"key: "<<key<<" "<<sizeof(key)<<endl;
+    // int16_t b[8]= {1,2,0,0,0,0,0,0};
+    b[0] = 'a';
+    b[1] = 'c';
+    b[3] = 'c';
+    b[4] = 'c';
+    b[5] = 'c';
+    b[6] = 'c';
+    b[7] = 0;
+    string test;
+
+    // memcpy(&test,&b,sizeof(b));
+    // cout<<test<<endl;
+    printArray(b);
+    /*
+        CREATE A PLAINTEXT FROM ARRAY OF SHORT INTEGERS -16 BIT
+        ENCRYPT,DECRYPT AND CHECK THE SCHEME WORKS. 
+    */
+    try
+	{
+		cout << "plain text: " << plain <<" "<<sizeof(plain)/sizeof(char) <<endl;
+
+		ECB_Mode< AES >::Encryption e;
+		e.SetKey(key, sizeof(key));
+
+		// The StreamTransformationFilter adds padding
+		//  as required. ECB and CBC Mode must be padded
+		//  to the block size of the cipher.
+		StringSource(plain, true, 
+			new StreamTransformationFilter(e,
+				new StringSink(cipher)
+			) // StreamTransformationFilter      
+		); // StringSource
+	}
+	catch(const CryptoPP::Exception& e)
+	{
+		cerr << e.what() << endl;
+		exit(1);
+	}
+
+	/*********************************\
+	\*********************************/
+
+	// Pretty print
+	encoded.clear();
+	StringSource(cipher, true,
+		new HexEncoder(
+			new StringSink(encoded)
+		) // HexEncoder
+	); // StringSource
+    cout<<sizeof(int)<<endl;
+    int c_arr[8];
+    memcpy(&c_arr,&cipher,sizeof(c_arr));
+    for (size_t i = 0; i < 8; i++)
+    {
+        cout<<c_arr[i]<<" ";
+    }
+    cout<<endl;
+	cout << "cipher text: " << encoded<<" "<<sizeof(cipher)/sizeof(char) << endl; 
+	/*********************************\
+	\*********************************/
+
+	try
+	{
+		ECB_Mode< AES >::Decryption d;
+		d.SetKey(key, sizeof(key));
+
+		// The StreamTransformationFilter removes
+		//  padding as required.
+		StringSource s(cipher, true, 
+			new StreamTransformationFilter(d,
+				new StringSink(recovered)
+			) // StreamTransformationFilter
+		); // StringSource
+
+		cout << "recovered text: " << recovered << endl;
+	}
+	catch(const CryptoPP::Exception& e)
+	{
+		cerr << e.what() << endl;
+		exit(1);
+	}
+    // prng.GenerateBlock(seed,sizeof(seed));
+    
     // encrypt here with AES
     
     // cout<<b<<endl;
     // cout<<sizeof(seed)<<" "<<sizeof(b)<<endl;
+    // for (i = 0; i < n; i++)
+    // {
+    //     for(j=0;j<m;j=j+8)
+    //     {
+    //         // b = {0,0,0,0,0,0,0,0};
+    //         b[0] = i;
+    //         b[1] = j;
+    //         cout<<b[0]<<" ";
+    //         printArray(b);
+            
+
+    //         // for (size_t k = 0; k < 8; k++)
+    //         // {
+    //         //     if (j+k<m)
+    //         //         A[i,j+k] = b[k]mod q
+    //         // }
+            
+    //     }
+    // }
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,27 +1,16 @@
-#include <climits>
-#include <iostream>
-#include "sodium.h"
 
+#include <iostream>
+#include <cstring>
 typedef long long dtype;
 using namespace std;
-dtype genUniformRandomlong(dtype lowerBound, dtype upperBound)
-{
-    dtype range = (upperBound - lowerBound) + 1;
-    dtype randomNumber;
-    randomNumber = randombytes_uniform(range);
-    dtype randomNumberModified = ((dtype)randomNumber) + lowerBound;
-    return randomNumberModified;
-}
 
 int main(int argc, char const *argv[])
 {
-  // dtupe temp = 1
-  size_t x = 1;
-  void * buf;
-  const unsigned char seed[1] = {'a'};
-  // void randombytes_buf_deterministic(void * const buf, const size_t size,
-  //                                  const unsigned char seed[randombytes_SEEDBYTES]);
-  cout<<randombytes_SEEDBYTES<<endl;
-  randombytes_buf_deterministic();
-  return 0;
+    string str;
+    int arr[5] = {2,4,6,8,10};
+    memcpy(&str,&arr,sizeof(arr)+1);
+    str[sizeof(arr)] = 0;
+    vector<byte> plain;
+    cout<<str;
+    return 0;
 }
