d11e866f0236fa4558a146b4ce176ca13f8ecb28
thushara weerasundara <e16388@eng.pdn.ac.lk>
2022-03-31 15:06:55 +0530

    Hash function for sha256 is implemented

---
+++ Hash/hash.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+#include <openssl/sha.h>
+unsigned long data_size;
+
+
+
+
+string sha256(unsigned char array[])
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, array, strlen((char*)array));
+    SHA256_Final(hash, &sha256);
+    stringstream ss;
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        ss << hex << setw(2) << setfill('0') << (int)hash[i];
+    }
+    return ss.str();
+}
+
+int main() 
+{
+    
+    
+
+    return 0;
+}
