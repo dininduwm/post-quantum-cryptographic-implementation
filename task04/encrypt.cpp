84fcf5b7ab85cdad25e50ceeace4258eb48b24d5
harshanabandara <e16049@eng.pdn.ac.lk>
2022-04-06 12:54:06 +0530

    instructions to task 4

---
+++ task04/encrypt.cpp
@@ -0,0 +1,13 @@
+/*
+    A(nxm) <-[0,q-1]    | public
+    b(mx1)  | public
+    D(mxk) <-{0,1}  | public
+    U(nxk) =AD  | public
+    H hash function | public
+*/
+
+/*
+    K key <- {0,1}
+    c1 = ENCRYPTaes_ctr(plain,k)
+    R(mxk) <- {0,1}
+*/
